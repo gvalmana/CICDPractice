6eaf3d02f95718c7dac91def6e1295e81ad2cd35
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2024-08-08 23:06:31 -0500

    commit

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,15 @@
-import type { Config } from '@jest/types'
+import type { Config } from "@jest/types";
 
-const baseDir = '<rootDir>/src/app/';
-const baseTestDir = '<rootDir>/src/test';
+const baseDir = "<rootDir>/src/app/";
+const baseTestDir = "<rootDir>/src/test";
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true,
-    collectCoverage: true,
-    collectCoverageFrom: [
-        `${baseDir}/**/*.ts`
-    ],
-    testMatch:[
-        `${baseTestDir}/**/*test.ts`
-    ]
-}
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  testMatch: [`${baseTestDir}/**/*test.ts`],
+};
 
 export default config;
